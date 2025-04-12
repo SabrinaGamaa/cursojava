62c4571d7da22366d5e475c33e7d293f47385dc9
Sabrina Gama <62630517+SabrinaGamaa@users.noreply.github.com>
2025-04-12 16:06:29 -0300

    Estudos

--- arquivos/src/aulas/LendoArquivo.java
+++ arquivos/src/aulas/LendoArquivo.java
@@ -1,5 +1,30 @@
 package aulas;
 
-public class LendoArquivo {
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class LendoArquivo {
+	public static void main(String[] args) {
+		
+		Scanner sc = null;
+		File file = new File("c:\\temp\\arquivoteste1.txt");
+		
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+			
+		}
+		catch (IOException e){
+			System.out.println("Error: " + e.getMessage());
+		}
+		
+		finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
+	}
 }
